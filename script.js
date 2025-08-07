e6e6443d2352fe38c96b93a008938c6d4e3679a0
Ryan Palmer <55644921+jedtherooster@users.noreply.github.com>
2025-08-08 09:36:11 +1200

    Finish Game

--- script.js
+++ script.js
@@ -1,2 +1,70 @@
-console.log("Hello, World!");
+let userScore = 0;
+let compScore = 0;
+let roundsPlayed = 0;
+
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    
+    switch (computerChoice) {
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3: 
+            return "scissors";
+            break;
+        default:
+            return "Error";
+    }
+}
+
+function getUserChoice() {
+    let userChoice = prompt("Enter your choice. || Rock || Paper || Scissors");
+    return userChoice;
+}
+
+function playRound(userChoice, compChoice) {
+    userChoice = userChoice.toLowerCase();
+
+    if (userChoice === compChoice) {
+        console.log("Tie");
+        playGame();
+    } else if (userChoice === "rock" && compChoice === "scissors") {
+        userScore++;
+        console.log("User wins!");
+        console.log(`User: |${userScore}| Computer: |${compScore}|`);
+        playGame();
+    } else if (userChoice === "paper" && compChoice === "rock") {
+        userScore++;
+        console.log("User wins!");
+        console.log(`User: |${userScore}| Computer: |${compScore}|`);
+        playGame();
+    } else if (userChoice === "scissors" && compChoice === "paper") {
+        userScore++;
+        console.log("User wins!");
+        console.log(`User: |${userScore}| Computer: |${compScore}|`);
+        playGame();
+    } else {
+        compScore++;
+        console.log("Computer wins!");
+        console.log(`User: |${userScore}| Computer: |${compScore}|`);
+        playGame();
+    }
+}
+
+function playGame() {
+    if (roundsPlayed <= 5) {
+        const userSelection = getUserChoice();
+        const computerSelection = getComputerChoice();
+        roundsPlayed++;
+        playRound(userSelection, computerSelection);
+    }
+}
+
+playGame();
+
+
+
 
